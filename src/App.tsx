aba38260b1b8e062adc29797d32539072c316374
CrunchyCat <calebrhoff@gmail.com>
2021-11-23 22:52:48 -0600

    Fist Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
 import './App.css';
 
-function App() {
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<Home />} />
+    </Routes>
+  );
+}
+
+const Home = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="cover-container d-flex h-100 p-3 mx-auto flex-column text-center">
+        <header className="masthead mb-auto">
+          <div className="inner">
+            <h3 className="masthead-brand">pickney</h3>
+            <nav className="nav nav-masthead justify-content-center">
+              <Link className="nav-link active" to="/">shop</Link>
+              <Link className="nav-link" to="/cart">cart</Link>
+              <Link className="nav-link" to="/login">login</Link>
+            </nav>
+          </div>
+        </header>
+        <main className="inner cover p-3" role="main">
+          <h1 className="cover-heading">shop for youth clothing.</h1>
+          <p className="lead">would you like to browse our wares? have a look around</p>
+          {/*TODO: Add Image Carousel */}
+          <hr/>
+          {/*TODO: Add Table of Clothing Items & Search */}
+        </main>
+        <footer className="mastfoot mt-auto">
+          <div className="inner">
+            <p>Created by <a href="http://www.calebhoff.com">Caleb Hoff</a> &amp; <a href="https://github.com/RaminNour77">Ramin Nourbakhsh</a></p>
+          </div>
+        </footer>
+      </div>
     </div>
   );
 }
