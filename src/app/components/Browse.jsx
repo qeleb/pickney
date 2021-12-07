c6e0d9341f82267b293d7344c431078e2277eb0b
RaminNour77 <46461137+RaminNour77@users.noreply.github.com>
2021-12-06 20:55:37 -0600

    pagination

--- src/app/components/Browse.jsx
+++ src/app/components/Browse.jsx
@@ -3,10 +3,21 @@ import { connect } from 'react-redux';
 import { ConnectedItemList } from './ItemList';
 
 const Browse = ({ groups }) => (
-    <div className="item-grid">
-        {groups.map(group => (
-            <ConnectedItemList key={group.id} {...group} />
-        ))}
+    <div className="browse">
+        <div className="item-grid">
+            {groups.map(group => (
+                <ConnectedItemList key={group.id} {...group} />
+            ))}
+        </div>
+            <nav aria-label="...">
+            <ul className="pagination justify-content-center pagination-sm">
+                <li className="page-item active" aria-current="page">
+                    <span className="page-link">1</span>
+                </li>
+                <li className="page-item"><a className="page-link" href="#">2</a></li>
+                <li className="page-item"><a className="page-link" href="#">3</a></li>
+            </ul>
+        </nav>
     </div>
 );
 
