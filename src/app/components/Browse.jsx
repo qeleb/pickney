cc83e635d75248e88d8fd94cc8bef236606f46ae
Caleb Hoff <crunchycat1@gmail.com>
2021-12-06 21:47:59 -0600

    pagination

--- src/app/components/Browse.jsx
+++ src/app/components/Browse.jsx
@@ -9,15 +9,6 @@ const Browse = ({ groups }) => (
                 <ConnectedItemList key={group.id} {...group} />
             ))}
         </div>
-            <nav aria-label="...">
-            <ul className="pagination justify-content-center pagination-sm">
-                <li className="page-item active" aria-current="page">
-                    <span className="page-link">1</span>
-                </li>
-                <li className="page-item"><a className="page-link" href="#">2</a></li>
-                <li className="page-item"><a className="page-link" href="#">3</a></li>
-            </ul>
-        </nav>
     </div>
 );
 
--- src/app/components/ItemList.jsx
+++ src/app/components/ItemList.jsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
+import ReactPaginate from 'react-paginate';
 import { requestItemCreation } from '../store/mutations'
 import { ConnectedItemListItem } from './ItemListItem'
 
+const ItemListPages = ({ name, items, id, isAdmin, createNewItem }) => {
+    const itemsPerPage = 2;
+    const [currentItems, setItems] = useState(items);
+    const [pageCount, setPageCount] = useState(0);
+    const [itemOffset, setItemOffset] = useState(0);
+
+    useEffect(() => {
+        // Fetch items from another resources.
+        const endOffset = itemOffset + itemsPerPage;
+        console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+        setItems(items.slice(itemOffset, endOffset));
+        setPageCount(Math.ceil(items.length / itemsPerPage));
+    }, [itemOffset, itemsPerPage]);
+
+    // Invoke when user click to request another page.
+    const handlePageClick = (event) => {
+        const newOffset = (event.selected * itemsPerPage) % items.length;
+        console.log(`User requested page number ${event.selected}, which is offset ${newOffset}`);
+        setItemOffset(newOffset);
+    };
+    return (
+        <div style={{display: 'flex', flexDirection: 'column'}}>
+            <ItemList name={name} items={currentItems} id={id} isAdmin={isAdmin} createNewItem={createNewItem} />
+            <ReactPaginate
+                style={{display: 'flex', justifyContent: 'center', flexDirection: 'row'}}
+                breakLabel="..."
+                nextLabel="next"
+                onPageChange={handlePageClick}
+                pageRangeDisplayed={5}
+                pageCount={pageCount}
+                previousLabel="previous"
+                renderOnZeroPageCount={null}
+            />
+        </div>
+    )
+};
+
 export const ItemList = ({ name, items, id, isAdmin, createNewItem }) => (
     <div className="p-2 m-2">
-        <div style={{display:'flex', justifyContent:'left'}}>
+        <div style={{ display: 'flex', justifyContent: 'left' }}>
             <h1>{name}</h1>
             {isAdmin && <button className="btn btn-secondary btn-block ms-3" onClick={() => createNewItem(id)}>+</button>}
         </div>
@@ -26,4 +64,4 @@ const mapDispatchToProps = (dispatch, { id }) => ({
     createNewItem: () => dispatch(requestItemCreation(id))
 });
 
-export const ConnectedItemList = connect(mapStateToProps, mapDispatchToProps)(ItemList);
+export const ConnectedItemList = connect(mapStateToProps, mapDispatchToProps)(ItemListPages);
